6736f3675c8bb225ecdf8c0b82054d5a422abfe8
Jeko0 <japulava@unisens.ge>
2022-10-14 22:27:30 +0400

    setted up calculation for reservation dates

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 class Property < ApplicationRecord
-  validates :name, presence: :true
-  validates :headline, presence: :true
-  validates :description, presence: :true
-  validates :address_1, presence: :true
-  validates :city, presence: :true
-  validates :state, presence: :true
-  validates :country, presence: :true
-  
+  validates :name, presence: true
+  validates :headline, presence: true
+  validates :description, presence: true
+  validates :address_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+
   has_many_attached :images, dependent: :destroy
 
   has_many :reviews, as: :reviewable
   has_many :favorites, dependent: :destroy
-  has_many :favorited_users, through: :favorites, source: :user  
-  has_many :reservations, dependent: :destroy 
+  has_many :favorited_users, through: :favorites, source: :user
+  has_many :reservations, dependent: :destroy
   has_many :reserved_users, through: :reservations, source: :user
 
   monetize :price_cents, allow_nil: true
-  
+
   geocoded_by :address
   after_validation :geocode, if: -> { latitude.blank? && longitude.blank? }
 
-  def address 
-    #[address_1, address_2, city, state, country].compact.join(', ') faker addresses are not real
+  def address
+    # [address_1, address_2, city, state, country].compact.join(', ') faker addresses are not real
     [state, country].compact.join(', ')
   end
 
-  def default_image 
+  def default_image
     images.first
   end
 
@@ -33,5 +35,14 @@ class Property < ApplicationRecord
     return if user.nil?
 
     favorited_users.include?(user)
+  end
+
+  def available_dates
+    date_format = '%b %e'
+
+    next_reservation = reservations.future_reservations.first
+    return Date.tomorrow.strftime(date_format)..Date.today.end_of_year.strftime(date_format) if next_reservation.nil?
+
+    Date.tomorrow.strftime(date_format)..next_reservation.reservation_date.strftime(date_format)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,6 @@ class Reservation < ApplicationRecord
   
   belongs_to :property
   belongs_to :user
+
+  scope :future_reservations, -> { where('reservation_date > ?', Date.today).order(:reservation_date)}
 end
