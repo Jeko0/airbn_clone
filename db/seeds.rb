1735fc5f4de7854c19e738d6881508f2deafabf5
Jeko0 <japulava@unisens.ge>
2022-10-27 16:25:56 +0400

    added reference and name to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+user_pictures = []
+
+6.times do 
+  user_pictures << URI.parse(Faker::LoremFlickr.image).open
+end
+
+me = User.create(email: "foo@bar.com", password: "password", first_name: "James", last_name: "Santos")
+me.picture.attach(io: user_pictures[0], filename: "#{me.full_name}.jpg")
+
+5.times do |i|
+  user = User.create(email: Faker::Internet.email, password: "password", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  user.picture.attach(io: user_pictures[i + 1], filename: "#{user.full_name}.jpg")
+end
+
 10.times do |i|
   property = Property.create(
     name: Faker::Lorem.word,
@@ -12,7 +26,7 @@
 
   property.images.attach(io: File.open(Rails.root.join("db", "sample", "images", "property_#{i + 1}.jpg")), filename: property.name)
 
-  (1..20).to_a.sample.times do 
-    Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph)
+  (1..10).to_a.sample.times do 
+    Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: User.all.sample)
   end
 end
