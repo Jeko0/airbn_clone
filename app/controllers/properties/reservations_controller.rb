1af7fb453ee4a87be08198a9a4cbec09656286ec
Jeko0 <japulava@unisens.ge>
2022-10-31 16:02:39 +0400

    passing parameters to likn

--- app/controllers/properties/reservations_controller.rb
+++ app/controllers/properties/reservations_controller.rb
@@ -5,14 +5,20 @@ module Properties
     before_action :authenticate_user!
 
     def new
-      @property = Property.find(params[:property_id])
-      @reservation = @property.reservations.new
+      @property      =  Property.find(params[:property_id])
+      @reservation   =  @property.reservations.new
+      @checkin_date  =  reservation_params[:checkin_date]
+      @checkout_date =  reservation_params[:checkout_date]
+      @nightly_total =  reservation_params[:nightly_total]
+      @cleaning_fee  =  reservation_params[:cleaning_fee]
+      @service_fee   =  reservation_params[:service_fee]
+      @total_cost    =  reservation_params[:total_cost]
     end
 
     private
 
     def reservation_params
-      params.permit(:checkin_date, :checkout_date, :nigthly_total, 
+      params.permit(:checkin_date, :checkout_date, :nightly_total,
                     :cleaning_fee, :service_fee, :total_cost)
     end
   end
